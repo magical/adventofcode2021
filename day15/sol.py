2075ce04674bb6191990c20c015f8ad0ff10f375
Andrew Ekstedt <andrew.ekstedt@gmail.com>
2021-12-15 13:33:23 -0800

    day 15 part 2

--- day15/sol.py
+++ day15/sol.py
@@ -1,4 +1,5 @@
 from collections import namedtuple
+from heapq import heappush, heappop
 
 def load(f):
     lines = [x.strip() for x in f if x.strip()]
@@ -7,14 +8,19 @@ def load(f):
     values = [list(map(int, x)) for x in lines]
     return Graph(width, height, values)
 
+MAX = 100000
+
 class Graph(namedtuple('Graph', "width, height, values")):
     def risk(G, pos):
         x,y = pos
-        if not 0 <= x < G.width:
-            return 1000
-        if not 0 <= y < G.height:
-            return 1000
-        return G.values[y][x]
+        bonus = 0
+        if not 0 <= x < 5*G.width:
+            return MAX
+        if not 0 <= y < 5*G.height:
+            return MAX
+        bonus = y//G.height + x//G.width
+        v = G.values[y%G.height][x%G.width]
+        return (v+bonus-1)%9 + 1
     def neighbors(G, pos):
         x, y = pos
         return [(x+1,y),(x-1,y),(x,y+1),(x,y-1)]
@@ -22,23 +28,33 @@ class Graph(namedtuple('Graph', "width, height, values")):
 def bestpath(G, start=(0,0), end=None):
     if end == None:
         end = (G.width-1, G.height-1)
-    queue = [(0,start)] # risk, pos
+    queue = [(distance(start,end),start)] # reduced risk, pos
     seen = set()
     while queue:
-        queue.sort()
-        c, here = queue.pop(0)
+        c, here = heappop(queue)
         if here in seen:
             continue
         #print(c, here)
         if here == end:
             return c
         seen.add(here)
+        k = distance(here,end)
         for n in G.neighbors(here):
             if n not in seen:
                 r = G.risk(n)
-                #print(c, r, here, n)
-                if r < 1000:
-                    queue.append((c+r, n))
+                if r < MAX:
+                    h = distance(n, end)
+                    heappush(queue, (c+r+h-k, n))
 
-print(bestpath(load(open('sample'))))
-print(bestpath(load(open('input'))))
+def distance(a,b):
+    """manhattan distance between points a and b"""
+    return abs(a[0]-b[0])+abs(a[1]-b[1])
+
+sample = load(open('sample'))
+input = load(open('input'))
+print(bestpath(sample))
+print(bestpath(input))
+#for y in range(sample.height*5):
+#    print(''.join(str(sample.risk((x,y))) for x in range(sample.width*5)))
+print(bestpath(sample, end=(sample.width*5-1, sample.height*5-1)))
+print(bestpath(input, end=(input.width*5-1, input.height*5-1)))
